52e3e046be192421505a941595a823dee3e22822
tu-usuario <afprietol@eafit.edu.co>
2025-02-07 00:09:30 -0500

    debugtruw

--- pinky_beauty_bar_project/settings.py
+++ pinky_beauty_bar_project/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "pinky_beauty_bar_project/static"),
